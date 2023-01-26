a9052df6b0da092aeba8c31ada3b59e802275e00
jaeminjeong <21900675@handong.edu>
2023-01-26 19:09:13 +0900

    create domain

---
+++ src/main/java/com/noriton/team9/domain/Item.java
@@ -0,0 +1,23 @@
+package com.noriton.team9.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "item")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
+    private String name;
+    private int price;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sample_id")
+    private Sample sample;
+}
