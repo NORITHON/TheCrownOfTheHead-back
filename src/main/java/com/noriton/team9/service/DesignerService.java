370a759d97eeb2e5f3d94c4f51f8fc9e28a49f14
jaeminjeong <21900675@handong.edu>
2023-01-27 02:36:27 +0900

    create login

--- src/main/java/com/noriton/team9/service/DesignerService.java
+++ src/main/java/com/noriton/team9/service/DesignerService.java
@@ -2,6 +2,7 @@ package com.noriton.team9.service;
 
 import com.noriton.team9.domain.Designer;
 import com.noriton.team9.domain.Item;
+import com.noriton.team9.domain.Member;
 import com.noriton.team9.domain.Sample;
 import com.noriton.team9.repository.DesignerRepository;
 import com.noriton.team9.repository.SampleRepository;
@@ -39,5 +40,22 @@ public class DesignerService {
         Designer designerToCreate = new Designer();
         BeanUtils.copyProperties(designer,designerToCreate);
         return designerRepository.save(designerToCreate);
+    }
+
+    public Designer login(String loginId, String password){
+        List<Designer> getDesigners = designerRepository.findByLoginId(loginId);
+        if(getDesigners.size() == 0) return new Designer();
+        int flag = 0;   // 비밀번호가 일치한 member가 없는 경우
+        int designerIdx=0;
+        for(int i=0; i<getDesigners.size(); i++){
+            if(getDesigners.get(i).getPassword().compareTo(password) == 0) {
+                flag = 1;
+                designerIdx = i;
+                break;
+            }
+        }
+
+        if(flag == 0) return new Designer();
+        return getDesigners.get(designerIdx);
     }
 }
