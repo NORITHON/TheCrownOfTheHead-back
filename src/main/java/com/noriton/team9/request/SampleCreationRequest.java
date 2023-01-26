81bb94bd02d478485932f4940e80e99e3308581e
jaeminjeong <21900675@handong.edu>
2023-01-26 20:32:50 +0900

    create service

---
+++ src/main/java/com/noriton/team9/request/SampleCreationRequest.java
@@ -0,0 +1,10 @@
+package com.noriton.team9.request;
+
+import lombok.Data;
+
+@Data
+public class SampleCreationRequest {
+    private String title;
+    private String imageUrl;
+    private String content;
+}
