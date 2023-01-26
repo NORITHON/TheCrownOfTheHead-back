e13610c93a3860f96d03140c371b477c592d33e6
jang-yujin <jyj9747@naver.com>
2023-01-26 19:39:47 +0900

    feat[#3]: order CR repository구현

--- src/main/java/com/noriton/team9/repository/OrderRepository.java
+++ src/main/java/com/noriton/team9/repository/OrderRepository.java
@@ -1,2 +1,31 @@
-package com.noriton.team9.repository;public class OrderRepository {
+package com.noriton.team9.repository;
+
+import com.noriton.team9.domain.Orders;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    /**
+     * 주문 생성
+     * */
+    public void save(Orders order){
+        em.persist(order);
+    }
+
+    /**
+     * 주문 전체 조회
+     * */
+    public List<Orders> findAll(){
+        return em.createQuery("select o from Orders o", Orders.class)
+                .getResultList();
+    }
+
 }
