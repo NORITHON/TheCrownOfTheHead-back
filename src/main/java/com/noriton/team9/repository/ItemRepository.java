f557974befeb51f686612ba42093791fea546639
yujin9747 <89755770+yujin9747@users.noreply.github.com>
2023-01-28 00:26:28 +0900

    Api연결 1 (#27)
    * docs[API#1]: 이미지 추가
    
    * fix[API#1]: 디자이너 request 필드 수정
    
    * feat[API#1]: 승인/미승인 item 조회 API개발

--- src/main/java/com/noriton/team9/repository/ItemRepository.java
+++ src/main/java/com/noriton/team9/repository/ItemRepository.java
@@ -3,5 +3,7 @@ package com.noriton.team9.repository;
 import com.noriton.team9.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
